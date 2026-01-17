c93eab8805abcc71429f34719265025549d1937b
Hyploh <cookiepuggie@gmail.com>
2026-01-17 23:20:13 +1100

    added the hammer

--- src/main/java/net/hypl/tutorialmod/TutorialMod.java
+++ src/main/java/net/hypl/tutorialmod/TutorialMod.java
@@ -2,15 +2,13 @@ package net.hypl.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.hypl.tutorialmod.block.ModBlocks;
 import net.hypl.tutorialmod.component.ModDataComponentTypes;
 import net.hypl.tutorialmod.item.ModItemGroups;
 import net.hypl.tutorialmod.item.ModItems;
-import net.minecraft.block.dispenser.DispenserBehavior;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.registry.Registry;
+import net.hypl.tutorialmod.util.HammerUsageEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +26,7 @@ public class TutorialMod implements ModInitializer {
 		ModDataComponentTypes.registerDataComponentTypes();
 
         FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES,600);
+
+		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
 	}
 }
--- src/main/java/net/hypl/tutorialmod/item/ModItems.java
+++ src/main/java/net/hypl/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.hypl.tutorialmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.hypl.tutorialmod.TutorialMod;
 import net.hypl.tutorialmod.item.custom.ChiselItem;
+import net.hypl.tutorialmod.item.custom.HammerItem;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
@@ -51,6 +52,10 @@ public class ModItems {
     public static final Item PINK_GARNET_HOE = registerItem("pink_garnet_hoe",
             new HoeItem(ModToolMaterials.PINK_GARNET, new Item.Settings()
                     .attributeModifiers(HoeItem.createAttributeModifiers(ModToolMaterials.PINK_GARNET,0, -3f))));
+
+    public static final Item PINK_GARNET_HAMMER = registerItem("pink_garnet_hammer",
+            new HammerItem((ModToolMaterials.PINK_GARNET), new Item.Settings()
+                    .attributeModifiers(PickaxeItem.createAttributeModifiers(ModToolMaterials.PINK_GARNET, 7, -3.4f))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
