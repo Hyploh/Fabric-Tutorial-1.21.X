b24d2ee50a39a0fa4b2dc9ac3dfa7430f118dedf
Hyploh <cookiepuggie@gmail.com>
2026-01-18 11:55:44 +1100

    added custom horse armor

--- src/main/java/net/hypl/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/hypl/tutorialmod/item/ModItemGroups.java
@@ -42,6 +42,8 @@ public class ModItemGroups {
                         entries.add(ModItems.PINK_GARNET_LEGGINGS);
                         entries.add(ModItems.PINK_GARNET_BOOTS);
 
+                        entries.add(ModItems.PINK_GARNET_HORSE_ARMOR);
+
 
                     }).build());
 
